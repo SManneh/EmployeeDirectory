9c0b3b0529f6539cd924e46761200fe2b684f791
Nabou <Nabou@Sainabou.local>
2018-09-25 18:04:43 -0500

    delete button works

--- 00-homework/unsolved/app.js
+++ 00-homework/unsolved/app.js
@@ -1,38 +1,44 @@
-
-const displayEmployees = function (){
-    $('.content').empty();
-    render();
-         
-}
-
-const clearTopElements = function(){
+//clears input fields to enable display of selected input function
+const clearTopElements = function () {
+    document.getElementById('displayElements').style.display = 'none';
     document.getElementById('addElements').style.display = 'none';
     document.getElementById('verifyElements').style.display = 'none';
     document.getElementById('updateElements').style.display = 'none';
+    document.getElementById('deleteElements').style.display = 'none';
 }
 
-const displayAddEmployee = function(){
+//clears contents from previous nav button display, and renders new display of employee list.
+const displayEmployees = function () {
+    $('.content').empty();
+    clearTopElements();
+    document.getElementById('displayElements').style.display = 'inline';
+    render();
+
+}
+
+//clears contents from previous nav button display, and renders new display of employee list.
+const displayAddEmployee = function () {
     $('.content').empty();
     clearTopElements();
     document.getElementById('addElements').style.display = 'inline';
     render();
 }
 
-const displayVerifyEmployee = function(){
+const displayVerifyEmployee = function () {
     $('.content').empty();
     clearTopElements()
     document.getElementById('verifyElements').style.display = 'inline';
     render();
 }
 
-const displayUpdateEmployee = function(){
+const displayUpdateEmployee = function () {
     $('.content').empty();
     clearTopElements()
     document.getElementById('updateElements').style.display = 'inline';
     render();
 }
 
-const displayDeleteEmployee = function(){
+const displayDeleteEmployee = function () {
     $('.content').empty();
     clearTopElements()
     document.getElementById('deleteElements').style.display = 'inline';
@@ -40,21 +46,22 @@ const displayDeleteEmployee = function(){
 }
 
 
-
-const render = function(){
+//displays employee list w/name, officenum, phonenum.
+const render = function () {
     for (let index = 0; index < employeeList.length; index++) {
         const employee = employeeList[index];
         var employeeName = employee.name + "<br/>";
         var employeeOfficeNum = employee.officeNum + "<br/>";
         var employeePhoneNum = employee.phoneNum + "<br/>";
         var employeeHtml = employeeName + employeeOfficeNum + employeePhoneNum;
-        $('.content').append(`<fieldset> ${employeeHtml} </fieldset>`);
-        
-        console.log(employee);
-    }   
-}
+        $('.content').append(`<fieldset> ${employeeHtml} </fieldset>`); // groups related elements in a form and 
+        //puts them in a box.
 
-const addEmployee = function(){
+        console.log(employee);
+    }
+}
+//user inputs name in values and the pushes it to employee list 
+const addEmployee = function () {
     var employee = {};
     //replace this with the Id of the elements in addElements div
     employee.name = $('#name').val();
@@ -63,55 +70,62 @@ const addEmployee = function(){
     employeeList.push(employee);
     displayEmployees();
 }
-
-const verifyEmployee = function(){
+//
+const verifyEmployee = function () {
     //read from the input name'
     var handle = $('#verifyname').val();
-    var result = employeeList.indexOfObject("name",handle);
+    var result = employeeList.indexOfObject("name", handle);
     $('.content').empty();
     displayEmployees();
-    if(result === -1){
+    if (result === -1) {
         $('.content').prepend("No");
     }
-    else{
+    else {
         $('.content').prepend("Yes");
     }
 }
-
+//gets the index of an employee in the employee list, returns -1 if employee does not exist
 Array.prototype.indexOfObject = function arrayObjectIndexOf(property, value) {
     for (var i = 0, len = this.length; i < len; i++) {
-        if (this[i][property] === value){
+        if (this[i][property] === value) {
             return i;
-        } 
+        }
     }
     return -1;
 }
 
-const updateEmployee = function(){
+//if input value === to any keys in the object, update the specific key in the object.
+const updateEmployee = function () {
     var employee = {};
-    //replace this with the Id of the elements in addElements div
-    const i = employeeList.findIndex( e => e.name === $('#updateName').val())
+    const i = employeeList.findIndex(e => e.name === $('#updateName').val())
     employeeList[i].officeNum = $('#updateOfficenum').val();
     employeeList[i].phoneNum = $('#updatePhonenum').val();
     displayEmployees();
 
 }
 
-const deleteEmployee = function(){
-    var employee = {};
-    //replace this with the Id of the elements in addElements div
-    const i = employeeList.findIndex( e => e.name === $('#deleteName').val())
-    employeeList.splice(i, 1);
+//if value inputed is equal to a name in employeelist, delete(splice)by 1.
+const deleteEmployee = function () {
+    let employee = $('#deleteName').val();//pass the input value to id name
+
+    for (let i = 0; i < employeeList.length; i++) {
+
+        if (employee === employeeList[i].name) {
+            employeeList.splice(i, 1)//delete employee by 1
+
+        }
+
+    }
     displayEmployees();
 }
 
 //displayEmployees();
 $("#view").on("click", displayEmployees); //display all employee
-$("#add").on("click", displayAddEmployee); //add an employee name, push into array
-$("#addEmp").on("click", addEmployee);
-$("#verify").on("click", displayVerifyEmployee);//verify if an employee is on the list
-$("#verifyEmp").on("click", verifyEmployee);//verify if an employee is on the list
-$("#update").on("click", displayUpdateEmployee);
-$("#updateEmp").on("click", updateEmployee); //update employee info 
-$("#delete").on("click", displayDeleteEmployee);//delete employee name from list bracket notation
-$("deleteEmp").on("click", deleteEmployee);
+$("#add").on("click", displayAddEmployee); //sidenav button displays all employee when clicked
+$("#addEmp").on("click", addEmployee);//button id to add an employee name already in value, push into array
+$("#verify").on("click", displayVerifyEmployee);//displays all employees when clicked
+$("#verifyEmp").on("click", verifyEmployee);//button id to verify if an employee is on the list
+$("#update").on("click", displayUpdateEmployee);//displays all employees when clicked
+$("#updateEmp").on("click", updateEmployee); //button id to update employee info in the input value
+$("#delete").on("click", displayDeleteEmployee);//displays all employees when clicked
+$("#deleteEmp").on("click", deleteEmployee);//button id to delete emp entered in input value
